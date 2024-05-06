7b03ba704c0054708149140f5ba8a7b7a016d57b
PDepaula <patrickgsdp@gmail.com>
2024-05-06 10:42:45 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+import pandas as pd
+from models import APIParameters, CoingeckoMarketSchema
+from typing import List
+from dataclasses import asdict
+
+url = 'https://api.coingecko.com/api/v3/coins/markets'
+
+def get_data(url: str, parameters: APIParameters) -> List[CoingeckoMarketSchema]:
+    """return paginated api response from url"""
+    input = parameters
+    result = []
+    while True:
+        response = requests.get(url, params=asdict(input))
+        response.raise_for_status()
+        data = response.json()
+        result.extend(data)
+        input.page += 1
+        if not data:
+            return result
+
+def process_data(data: List[CoingeckoMarketSchema]) -> pd.DataFrame:
+    data_dicts = []
+    for obj in data:
+        market_data = CoingeckoMarketSchema(**obj)
+        data_dicts.append(asdict(market_data))
+    
+    dataframe = pd.DataFrame(data_dicts)
+    print(dataframe)
+    return dataframe
+
+
+def main():
+    data = get_data(url,APIParameters())
+    process_data(data)
+
+main()
---
+++ models.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class CoingeckoMarketSchema:
+    id: str
+    symbol: str
+    name: str
+    image: str
+    current_price: float
+    market_cap: int
+    market_cap_rank: int
+    fully_diluted_valuation: int
+    total_volume: int
+    high_24h: int
+    low_24h: int
+    price_change_24h: float
+    price_change_percentage_24h: float
+    market_cap_change_24h: int
+    market_cap_change_percentage_24h: int
+    circulating_supply: int
+    total_supply: int
+    max_supply: int
+    ath: float
+    ath_change_percentage: float
+    ath_date: str
+    atl: float
+    atl_change_percentage: float
+    atl_date: str
+    roi: dict
+    last_updated: str
+
+@dataclass
+class APIParameters:
+    vs_currency: Optional[str] = 'usd'
+    ids: Optional[str] = None
+    category: Optional[str] = "centralized-exchange-token-cex"
+    order: Optional[str] = None
+    per_page: Optional[int] = 250
+    page: Optional[int] = 1
+    sparkline: Optional[bool] = False
+    locale: Optional[str] = None
+    precision: Optional[str] = None
